d3a997bc338e44368a110cd807f45c2e6eac3a60
Lamia Haider <lamia.haider@gmail.com>
2020-02-04 02:45:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,21 +2,19 @@
 def reverse_each_word(sentence)
   normal = sentence.split(" ")
   reversed = []
-<<<<<<< HEAD
+
   normal.collect do |word|
-=======
+
   normal.each do |word|
->>>>>>> 8ffd8e7836c3c4a7b37b0d7e505e067d2ca6ca01
+
   reversed << word.reverse
   end
   reversed.join(" ")
 end
-<<<<<<< HEAD
-=======
+
 
 def reverse_each_word2(sentence)
   normal = sentence.split(" ")
   normal.collect {|word| word.reverse}
   normal.join("")
 end
->>>>>>> 8ffd8e7836c3c4a7b37b0d7e505e067d2ca6ca01
